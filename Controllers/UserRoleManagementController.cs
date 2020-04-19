1d016f1194a5c5e8a3741161ee4e1d900a4ae41e
upretymadan45@gmail.com <upretymadan45@gmail.com>
2020-04-19 19:43:29 +0545

    project upload

---
+++ Controllers/UserRoleManagementController.cs
@@ -0,0 +1,104 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using authdemo.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace authdemo.Controllers
+{
+    [Authorize]
+    public class UserRoleManagementController : Controller
+    {
+        private readonly UserManager<IdentityUser> userManager;
+        private readonly RoleManager<IdentityRole> roleManager;
+
+        public UserRoleManagementController(UserManager<IdentityUser> userManager,
+                                            RoleManager<IdentityRole> roleManager)
+        {
+            this.userManager = userManager;
+            this.roleManager = roleManager;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var users = userManager.Users.ToList();
+            return View(users);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Details(string userId)
+        {
+            var user = await userManager.FindByIdAsync(userId);
+
+            ViewBag.UserName = user.UserName;
+
+            var userRoles = await userManager.GetRolesAsync(user);
+
+            return View(userRoles);
+        }
+
+        [HttpGet]
+        public IActionResult AddRole()
+        {
+            return RedirectToAction(nameof(DisplayRoles));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddRole(string role)
+        {
+            await roleManager.CreateAsync(new IdentityRole(role));
+            return RedirectToAction(nameof(DisplayRoles));
+        }
+
+        [HttpGet]
+        public IActionResult DisplayRoles()
+        {
+            var roles = roleManager.Roles.ToList();
+           
+            return View(roles);
+        }
+
+        [HttpGet]
+        public IActionResult AddUserToRole()
+        {
+            var users = userManager.Users.ToList();
+            var roles = roleManager.Roles.ToList();
+
+            ViewBag.Users = new SelectList(users, "Id", "UserName");
+            ViewBag.Roles = new SelectList(roles, "Name", "Name");
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddUserToRole(UserRole userRole)
+        {
+            var user = await userManager.FindByIdAsync(userRole.UserId);
+
+            await userManager.AddToRoleAsync(user, userRole.RoleName);
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> RemoveUserRole(string role,string userName)
+        {
+            var user = await userManager.FindByNameAsync(userName);
+
+            var result = await userManager.RemoveFromRoleAsync(user, role);
+
+            return RedirectToAction(nameof(Details),new { userId = user.Id });
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> RemoveRole(string role)
+        {
+           var roleToDelete = await roleManager.FindByNameAsync(role);
+           var result = await roleManager.DeleteAsync(roleToDelete);
+
+            return RedirectToAction(nameof(DisplayRoles));
+        }
+    }
+}
